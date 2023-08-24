ad37a8383a281532d8466680af0c125b2a84f181
PatilSandhya <begadesandhya@gmail.com>
2023-08-24 22:55:21 +0530

    update

--- src/Components/CreditReport/PersonalInfo.jsx
+++ src/Components/CreditReport/PersonalInfo.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 export const PersonalInfo = () => {
+  const [formdata, setformData] = useState({
+    name:"",
+    lName:"",
+    mobile:"",
+    pan:"",
+    dob:"",
+    isDate:"",
+    email:"",
+    password:"",
+    gender:""
+  })
   return (
     <div>PersonalInfo</div>
   )
